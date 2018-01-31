6ada6406ce441c06ae9638627d18adf62c9fa269
TheChrisED <>
2018-01-31 23:51:11 +0100

    follow-rotation reacts to x,y,z axis input; pip-video-interface added

--- video-player/pip-video-components.js
+++ video-player/pip-video-components.js
@@ -1,23 +1,51 @@
 AFRAME.registerComponent('follow-rotation', {
   schema: {
     entity: {type: "selector"},
-    x: {default: true},
+    x: {default: false},
     y: {default: true},
-    z: {default: true},
+    z: {default: false},
   },
   init: function () {
-    this.el.setAttribute("position", this.data.entity.components.position.data);
+    //this.el.setAttribute("position", this.data.entity.components.position.data);
+    this.followRotation();
   },
   play: function () {
   },
   pause: function () {         
   },
   tick: function (time, deltaTime) {
+    this.followRotation();
+  },
+  followRotation: function() {
     var newRotation = {
-      x: this.el.components.rotation.data.x,
-      y: this.data.entity.components.rotation.data.y,
-      z: this.el.components.rotation.data.z,
+      x: this.data.x? this.data.entity.components.rotation.data.x: this.el.components.rotation.data.x,
+      y: this.data.y? this.data.entity.components.rotation.data.y: this.el.components.rotation.data.y,
+      z: this.data.z? this.data.entity.components.rotation.data.z:this.el.components.rotation.data.z,
     };
     this.el.setAttribute("rotation", newRotation);
+  }
+});
+
+AFRAME.registerComponent('pip-video-interface', {
+  schema: {
+    video2d: {type: "selector"}
+  },
+  init: function () {
+    console.log("pip-video-interface initialized", this.data.video2d);
+    var width = this.data.video2d.getAttribute("width");
+    var height = this.data.video2d.getAttribute("height");
+    var multiplier = 1;
+    console.log("width: ", width);
+    width *= multiplier;
+    height *= multiplier;
+    console.log("new width: ", width);
+    this.data.video2d.setAttribute("width", width);
+    this.data.video2d.setAttribute("height", height);
+  },
+  play: function () {
+  },
+  pause: function () {         
+  },
+  tick: function (time, deltaTime) {
   }
 });
